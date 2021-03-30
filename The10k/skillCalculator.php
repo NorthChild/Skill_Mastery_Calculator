7ea8b1b3a15986a022c24f6251310e3cd374f4e5
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-30 19:20:11 +0200

    Update skillCalculator.php

--- The10k/skillCalculator.php
+++ The10k/skillCalculator.php
@@ -1,1 +1,20 @@
-<?php  // code coming soon ?>
+<?php  // code coming soon
+
+
+session_start();
+
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+
+  $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+  $stmt = $pdo -> prepare($sql);
+  $stmt -> execute(array(
+    ':name' => $_POST['name'],
+    ':email' => $_POST['email'],
+    ':password' => $_POST['password'],
+  ));
+  $_SESSION['success'] = 'Record Added';
+  header('Location: CRUD.php');
+  return;
+}
+
+?>
